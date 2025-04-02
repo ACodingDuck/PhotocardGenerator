bba7fbdb14855c860d27ef73e65ce98522d082c2
ACodingDuck <185631838+ACodingDuck@users.noreply.github.com>
2025-04-01 21:31:25 -0700

    edit apr 1

--- main.js
+++ main.js
@@ -1,22 +1,70 @@
-let attack = 0;
-let defense = 0;
-let speed = 0;
-
 const card = document.querySelector('#card');
 const btn = document.querySelector('#btn');
-const idolName = document.querySelector('#idol-name');
-const hp = document.querySelector('#hp');
-const type = document.querySelector('#type');
-const attackStat = document.querySelector('#attack');
-const defenseStat = document.querySelector('#defense');
-const speedStat = document.querySelector('#speed');
+
+let idols = ['Sakura', 'Yunjin', 'Eunchae', 'Chaewon', 'Kazuha'];
+let hp = ['80', '90', '100', '110', '120'];
+let attack = ['50', '60', '70', '80', '90'];
+let defense = ['40', '50', '60', '70', '80'];
+let speed = ['30', '40', '50', '60', '70'];
+let imgs = ['imgs/yunjin1.jpg', 'imgs/chaewon1.jpg', 'imgs/eunchae1.jpg', 'imgs/kazuha.jpg', 'imgs/sakura1.jpg'];
 
 
-function getRandomIdol() {
-    const idolNames = ["Sakura", "Yuki", "Hana", "Riku", "Aiko"];
-    const randomIndex = Math.floor(Math.random() * idolNames.length);
-    return idolNames[randomIndex];
-}
+let getRandomIdol = () => {
+    let id = idols[Math.floor(Math.random() * idols.length)];
+    if (id === 'Sakura') {
+        img1 = 'imgs/sakura1.jpg';
+    } else if (id === 'Yunjin') {
+        img1 = 'imgs/yunjin1.jpg';
+    } else if (id === 'Eunchae') {
+        img1 = 'imgs/eunchae1.jpg';
+    }
+    else if (id === 'Chaewon') {
+        img1 = 'imgs/chaewon1.jpg';
+    }
+    else if (id === 'Kazuha') { 
+        img1 = 'imgs/kazuha.jpg';
+    };
+    let hp1 = hp[Math.floor(Math.random() * hp.length)];
+    let attack1 = attack[Math.floor(Math.random() * attack.length)];
+    let defense1 = defense[Math.floor(Math.random() * defense.length)];
+    let speed1 = speed[Math.floor(Math.random() * speed.length)];
+   
+
+    const finalCard = id + ' ' + hp1 + ' ' + attack1 + ' ' + defense1 + ' ' + speed1;
+    
+    console.log(finalCard);
+    
+    card.innerHTML = `
+         <p class="hp">
+            <span>HP</span>
+            <span>${hp1}</span>
+          </p>
+        <img src=${img1} alt="">
+        <h2 class="idol-name">${id}</h2> 
+        <div class="types">
+          <span>type 1</span>
+          <span>type 2</span>
+        </div>
+        <div class="stats">
+          <div>
+            <h3>${attack1}</h3>
+            <p>Attack</p>
+            </div> 
+            <div>
+              <h3>${defense1}</h3>
+              <p>Defense</p>
+              </div> 
+              <div>
+                <h3>${speed1}</h3>
+                <p>Speed</p>
+                </div>
+        </div>
+    `;
+};
+     
+        
+    /* card.innerHTML = finalCard; */
 
 
 btn.addEventListener('click', getRandomIdol);
+window.addEventListener('load', getRandomIdol);
